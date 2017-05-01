fa14037fddbb8e2479df2daf5e357d270dd75247
Santiago Rodríguez <santi@collazo.eu>
2017-05-01 19:18:19 +0000

    Add packer configuration files
    Based on work from @sevein and github.com/chef/bento

---
+++ packer/scripts/common/virtualbox.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -eux
+
+case "$PACKER_BUILDER_TYPE" in
+virtualbox-iso|virtualbox-ovf)
+    VER="`cat /home/vagrant/.vbox_version`";
+    ISO="VBoxGuestAdditions.iso";
+
+    echo "Working around regression in 5.1.20 guest additions";
+    echo "So we're using an updated test build";
+    echo "It's probably fine";
+    echo "Virtualbox Tools Version: 5.1.21";
+
+    mkdir -p /tmp/vbox;
+    mount -o loop $HOME_DIR/$ISO /tmp/vbox;
+    sh /tmp/vbox/VBoxLinuxAdditions.run || true
+    umount /tmp/vbox || true
+    rm -rf /tmp/vbox || true
+    rm -f $HOME_DIR/*.iso || true
+    ;;
+esac
+echo acaba el script
