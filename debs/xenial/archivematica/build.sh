9e61e524638d01896581846a7b0087c7fa698200
Santiago Rodríguez <santi@collazo.eu>
2017-07-28 16:24:02 +0200

    debs: archivematica xenial package

---
+++ debs/xenial/archivematica/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+BASE="$(pwd)"
+SOURCE=${BASE}/src/archivematica/src/
+export DEBFULLNAME="Artefactual Systems"
+export DEBEMAIL="sysadmin@artefactual.com"
+
+cd $SOURCE
+BRANCH="$(git branch | cut -d\  -f2-)"
+COMMIT=$(git rev-parse HEAD)
+
+
+# Update changelog for xenial
+for i in dashboard MCPClient MCPServer archivematicaCommon
+	do
+	cd "${SOURCE}/$i/"
+	dch -v 1:${VERSION}${RELEASE} commit: $(echo $COMMIT)
+	dch -v 1:${VERSION}${RELEASE} checkout: $(echo $BRANCH) 
+	dch -r --distribution xenial --urgency high ignored		
+	cp $BASE/debian-$i/* debian/
+	dpkg-buildpackage -us -uc
+	cd $SOURCE
+	done
+
