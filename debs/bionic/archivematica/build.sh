edbde62a8244bfc31db23705041b01413c4e00a2
Santiago Rodríguez <santi@collazo.eu>
2018-10-10 02:09:08 +0200

    bionic: add archivematica package

---
+++ debs/bionic/archivematica/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+BASE="$(pwd)"
+SOURCE=${BASE}/src/archivematica/src/
+export DEBFULLNAME="Artefactual Systems"
+export DEBEMAIL="sysadmin@artefactual.com"
+
+for j in transfer-browser appraisal-tab
+  do
+    cd $SOURCE/dashboard/frontend/$j/
+    npm install --unsafe-perm
+  done
+
+cd $SOURCE
+COMMIT=$(git rev-parse HEAD)
+
+# Update changelog for bionic
+for i in dashboard MCPClient MCPServer archivematicaCommon
+	do
+	cd "${SOURCE}/$i/"
+	cp -rf $BASE/debian-$i debian
+	yes | mk-build-deps -i debian/control
+	dch -v 1:${VERSION}${RELEASE}~18.04 commit: $(echo $COMMIT)
+	dch -v 1:${VERSION}${RELEASE}~18.04 checkout: $(echo $BRANCH) 
+	dch -r --distribution bionic --urgency high ignored		
+	QUILT_PATCHES="debian/patches" quilt push -a || true
+	dpkg-buildpackage -us -uc
+	cd $SOURCE
+	done
+
