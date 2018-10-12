5aa6a8ec8e4bbc250b6bbb0c36dfb99a61b13747
Santiago Rodríguez <santi@collazo.eu>
2018-10-12 15:14:39 +0200

    bionic: add ss package

---
+++ debs/bionic/archivematica-storage-service/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+BASE="$(pwd)"
+SOURCE=${BASE}/src/archivematica-storage-service/
+export DEBFULLNAME="Artefactual Systems"
+export DEBEMAIL="sysadmin@artefactual.com"
+
+cd $SOURCE
+COMMIT=$(git rev-parse HEAD)
+echo $BASE
+cp -rf ${BASE}/debian-storage-service debian
+QUILT_PATCHES="debian/patches" quilt push -a || true
+pip download -d lib --no-binary :all: -r requirements.txt
+yes | mk-build-deps -i debian/control
+dch -v 1:${VERSION}${RELEASE}~18.04 commit: $(echo $COMMIT)
+dch -v 1:${VERSION}${RELEASE}~18.04 checkout: $(echo $BRANCH) 
+dch -r --distribution bionic --urgency high ignored		
+dpkg-buildpackage -us -uc
+cd $SOURCE
+
