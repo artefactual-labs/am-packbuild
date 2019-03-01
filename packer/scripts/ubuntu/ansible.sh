2e9fad6c18eacf1303694800fb2917913312c7fd
Santiago Rodríguez <santi@collazo.eu>
2019-03-01 21:32:58 +0100

    packer: update am box for 1.9

--- packer/scripts/ubuntu/ansible.sh
+++ packer/scripts/ubuntu/ansible.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -eux
 
-apt-get install -y software-properties-common python-software-properties
-apt-add-repository ppa:ansible/ansible
 apt-get update
-apt-get install -y ansible
+apt-get install -y ansible git
