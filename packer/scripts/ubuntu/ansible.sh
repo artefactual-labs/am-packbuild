d8a85a933c0f624b051e0a2058e5945ebfd1af45
Santiago Rodríguez <santi@collazo.eu>
2019-03-04 13:20:06 +0100

    packer: install net-tools package
    The package provides /sbin/ifconfig, used by some VBox versions for
    network configuration

--- packer/scripts/ubuntu/ansible.sh
+++ packer/scripts/ubuntu/ansible.sh
@@ -2,7 +2,7 @@
 
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y ansible git
+apt-get install -y ansible git net-tools
 
 echo '[defaults]' > /etc/ansible/ansible.cfg
 echo 'allow_world_readable_tmpfiles = True' >> /etc/ansible/ansible.cfg
