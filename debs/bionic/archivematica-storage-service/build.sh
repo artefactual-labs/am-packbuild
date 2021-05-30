efffac343df2f6fc8beb8e918c1d4a6b2eb522ca
Jesús García Crespo <jesus@sevein.com>
2021-05-30 14:46:10 +0200

    Use Python 3.6 in AM/SS RPM packages
    * Install dh-virtualenv 1.2 from author's PPA
    * Use Python 3.6 and new requirements file
    * Update PYTHONPATH of MCPClient
    * Remove workarounds for Python 2.x
    * Remove workarounds for old releases
    * Remove quilt (no patches needed)
    * Remove unused build variables
    * Introduce archivematica placeholder package (deb) with virtualenv
    * Stop configuring packages.archivematica.org

--- debs/bionic/archivematica-storage-service/build.sh
+++ debs/bionic/archivematica-storage-service/build.sh
@@ -1,15 +1,16 @@
-#!/bin/bash -x
+#!/usr/bin/env bash
+
+set -euxo
 
 BASE="$(pwd)"
 SOURCE=${BASE}/src/archivematica-storage-service/
 export DEBFULLNAME="Artefactual Systems"
 export DEBEMAIL="sysadmin@artefactual.com"
+export DEB_BUILD_OPTIONS="noddebs"
 
 cd $SOURCE
 COMMIT=$(git rev-parse HEAD)
 cp -rf ${BASE}/debian-storage-service debian
-QUILT_PATCHES="debian/patches" quilt push -a || true
-pip download -d lib --no-binary :all: -r requirements.txt
 yes | mk-build-deps -i debian/control
 dch -v 1:${VERSION}${RELEASE}~18.04 commit: $(echo $COMMIT)
 dch -v 1:${VERSION}${RELEASE}~18.04 checkout: $(echo $BRANCH) 
