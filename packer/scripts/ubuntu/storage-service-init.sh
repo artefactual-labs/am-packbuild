80f4bdb17974d82db2fc749ba93a2d9764e99e5d
Jesús García Crespo <jesus@sevein.com>
2021-06-24 08:43:23 +0000

    Remove storage-service-init.sh
    This is now a config provisioned by the Ansible role.

--- packer/scripts/ubuntu/storage-service-init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -eux
-
-# Restart archivematica-storage-service on failure (helps on boot)
-sed -i 's/User=archivematica/Restart=on-failure\nRestartSec=10\nUser=archivematica/g' /etc/systemd/system/archivematica-storage-service.service
-
