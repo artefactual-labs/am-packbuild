9aae1e0c07963a6e33a57c8e9d5851e9e4129c7f
Dhwani Patel <dpatel@artefactual.com>
2023-10-16 12:48:16 -0600

    Add jammy deb package for AM and SS

---
+++ debs/jammy/archivematica-storage-service/changelog.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export DEBFULLNAME="Artefactual Systems"
+export DEBEMAIL="sysadmin@artefactual.com"
+BRANCH="$(git branch | cut -d\  -f2-)"
+COMMIT=$(git rev-parse HEAD)
+
+dch -v 1:${VERSION}${RELEASE} commit: $(echo $COMMIT)
+dch -v 1:${VERSION}${RELEASE} checkout: $(echo $BRANCH) 
+dch -r --distribution jammy --urgency high ignored
