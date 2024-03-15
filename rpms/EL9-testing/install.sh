0587b3a70a566f4f1696f6075ba5eb3d6dba4b77
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2024-03-15 05:43:47 +0100

    Update EL9 testing script

--- rpms/EL9-testing/install.sh
+++ rpms/EL9-testing/install.sh
@@ -53,12 +53,12 @@ enabled=1
 EOF'
 fi
 
-sudo -u root bash -c 'cat << EOF >> /etc/yum.repos.d/archivematica.repo
+sudo -u root bash -c 'cat << EOF >> /etc/yum.repos.d/archivematica-extras.repo
 [archivematica-extras]
 name=archivematica-extras
 baseurl=https://packages.archivematica.org/1.16.x/rocky9-extras
 gpgcheck=1
-gpgkey=https://packages.archivematica.org/1.16.x/key.asc
+gpgkey=https://packages.archivematica.org/GPG-KEY-archivematica-sha512
 enabled=1
 EOF'
 
@@ -111,7 +111,7 @@ fi
 # Archivematica Storage Service
 #
 
-sudo -H -u root mysql -hlocalhost -uroot -e "DROP DATABASE IF EXISTS SS; CREATE DATABASE SS CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+sudo -H -u root mysql -hlocalhost -uroot -e "DROP DATABASE IF EXISTS SS; CREATE DATABASE SS CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
 sudo -H -u root mysql -hlocalhost -uroot -e "CREATE USER 'archivematica'@'localhost' IDENTIFIED BY 'demo';"
 sudo -H -u root mysql -hlocalhost -uroot -e "GRANT ALL ON SS.* TO 'archivematica'@'localhost';"
 
@@ -138,7 +138,7 @@ sudo -u root systemctl start rngd
 sudo -u root yum clean all
 sudo -u root yum install -y archivematica-common archivematica-mcp-server archivematica-dashboard
 
-sudo -H -u root mysql -hlocalhost -uroot -e "DROP DATABASE IF EXISTS MCP; CREATE DATABASE MCP CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+sudo -H -u root mysql -hlocalhost -uroot -e "DROP DATABASE IF EXISTS MCP; CREATE DATABASE MCP CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
 sudo -H -u root mysql -hlocalhost -uroot -e "GRANT ALL ON MCP.* TO 'archivematica'@'localhost';"
 
 sudo -u archivematica bash -c " \
