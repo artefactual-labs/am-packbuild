c450bff96048b70f9718efb55c41133fb63fd6ca
Douglas Cerna <douglascerna@yahoo.com>
2024-04-04 15:33:04 -0600

    AM 1.16.0 + SS 0.22.0
    * Upgrade pre-commit dependencies
    * Upgrade test workflow dependencies
    * debs: Use dbconfig in jammy SS packages and others
    * Update debhelper compat to 10
    * Allow to use external database server in AM and SS
    * Update 1.15 references
    * Update 0.21 references
    * Update RPM testing script
    * Update DEB testing script
    * Add GitHub workflows for AM packages
    
    Co-authored-by: Miguel Medinilla <mamedin@artefactual.com>

--- debs/jammy-testing/install.sh
+++ debs/jammy-testing/install.sh
@@ -30,25 +30,34 @@ while read -r line; do echo "$line=${!line}"; done < <(compgen -v | grep -v '[^[
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
 export DEBIAN_FRONTEND=noninteractive
+sudo debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
+sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+sudo debconf-set-selections <<< "archivematica-storage-service archivematica-storage-service/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "archivematica-storage-service archivematica-storage-service/mysql/app-pass password demo-ss"
+sudo debconf-set-selections <<< "archivematica-storage-service archivematica-storage-service/app-password-confirm password demo-ss"
+sudo debconf-set-selections <<< "archivematica-mcp-server archivematica-mcp-server/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "archivematica-mcp-server archivematica-mcp-server/mysql/app-pass password demo-am"
+sudo debconf-set-selections <<< "archivematica-mcp-server archivematica-mcp-server/app-password-confirm password demo-am"
 
-sudo wget -O - https://packages.archivematica.org/1.15.x/key.asc | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://packages.archivematica.org/1.15.x/ubuntu-externals jammy main" >> /etc/apt/sources.list'
+curl -fsSL https://packages.archivematica.org/1.16.x/key.asc | sudo gpg --dearmor -o /etc/apt/keyrings/archivematica-1.16.x.gpg
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/archivematica-1.16.x.gpg] http://packages.archivematica.org/1.16.x/ubuntu-externals jammy main" > /etc/apt/sources.list.d/archivematica-externals.list'
 
 if [ "${local_repository}" == "true" ] ; then
     sudo -u root bash -c 'cat << EOF > /etc/apt/sources.list.d/archivematica.list
 deb file:/am-packbuild/debs/jammy/_deb_repository ./
 EOF'
 else
-    sudo sh -c 'echo "deb [arch=amd64] http://packages.archivematica.org/1.15.x/ubuntu jammy main" >> /etc/apt/sources.list'
+    sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/archivematica-1.16.x.gpg] http://packages.archivematica.org/1.16.x/ubuntu jammy main" > /etc/apt/sources.list.d/archivematica.list'
 fi
 
 sudo apt-get -o Acquire::AllowInsecureRepositories=true update
 sudo apt-get -y upgrade
-sudo apt-get install -y htop ntp apt-transport-https unzip openjdk-8-jre-headless mysql-server python3-mysqldb
+
+sudo apt-get install -y openjdk-8-jre-headless mysql-server
 
 if [ "${search_enabled}" == "true" ] ; then
-    wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-    echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+    curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /etc/apt/keyrings/elasticsearch-6.x.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/elasticsearch-6.x.gpg] https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
     sudo apt-get -o Acquire::AllowInsecureRepositories=true update
     sudo apt-get install -y elasticsearch
     sudo systemctl daemon-reload
@@ -56,20 +65,10 @@ if [ "${search_enabled}" == "true" ] ; then
     sudo systemctl enable elasticsearch
 fi
 
-sudo -H -u root mysql -hlocalhost -uroot -e "DROP DATABASE IF EXISTS SS; CREATE DATABASE SS CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
-sudo -H -u root mysql -hlocalhost -uroot -e "CREATE USER 'archivematica'@'localhost' IDENTIFIED BY 'demo';"
-sudo -H -u root mysql -hlocalhost -uroot -e "GRANT ALL ON SS.* TO 'archivematica'@'localhost';"
-
 sudo apt-get install -y --allow-unauthenticated archivematica-storage-service
 
 sudo rm -f /etc/nginx/sites-enabled/default
 sudo ln -sf /etc/nginx/sites-available/storage /etc/nginx/sites-enabled/storage
-
-sudo -H -u root mysql -hlocalhost -uroot -e "DROP DATABASE IF EXISTS MCP; CREATE DATABASE MCP CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
-sudo -H -u root mysql -hlocalhost -uroot -e "GRANT ALL ON MCP.* TO 'archivematica'@'localhost';"
-
-echo archivematica-mcp-server archivematica-mcp-server/dbconfig-install boolean false | sudo debconf-set-selections
-echo postfix postfix/main_mailer_type select No configuration | sudo debconf-set-selections
 
 sudo apt-get install -y --allow-unauthenticated archivematica-mcp-server
 sudo apt-get install -y --allow-unauthenticated archivematica-dashboard
@@ -94,3 +93,36 @@ sudo service archivematica-dashboard restart
 sudo service nginx restart
 sudo systemctl enable fits-nailgun
 sudo service fits-nailgun start
+
+sudo -u archivematica bash -c " \
+    set -a -e -x
+    source /etc/default/archivematica-storage-service || \
+        source /etc/sysconfig/archivematica-storage-service \
+            || (echo 'Environment file not found'; exit 1)
+    cd /usr/lib/archivematica/storage-service
+      /usr/share/archivematica/virtualenvs/archivematica-storage-service/bin/python manage.py create_user \
+          --username=admin \
+          --password=archivematica \
+          --email="example@example.com" \
+          --api-key="apikey" \
+          --superuser
+";
+
+sudo -u archivematica bash -c " \
+    set -a -e -x
+    source /etc/default/archivematica-dashboard || \
+        source /etc/sysconfig/archivematica-dashboard \
+            || (echo 'Environment file not found'; exit 1)
+    cd /usr/share/archivematica/dashboard
+      /usr/share/archivematica/virtualenvs/archivematica/bin/python manage.py install \
+          --username="admin" \
+          --password="archivematica" \
+          --email="example@example.com" \
+          --org-name="test" \
+          --org-id="test" \
+          --api-key="apikey" \
+          --ss-url="http://localhost:8000" \
+          --ss-user="admin" \
+          --ss-api-key="apikey" \
+          --site-url="http://localhost"
+";
